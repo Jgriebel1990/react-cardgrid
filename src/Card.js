5c9d17cac89bd22fb778dc022162c072a9f3303e
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-09 18:18:36 -0500

    added ternary operator to conditionally select style and classes to the card

--- src/Card.js
+++ src/Card.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 
 class Card extends Component {
     render(){
-        let style;
-        if (this.props.isLiked){
-            style = { color: "chartreuse"}
+        //look up and think about how we can use the "ternary" operator
+        //it is sometimes know as the "elvis" operator
+
+        //using the ternary operator
+        const style = {
+            color: this.props.isLiked ? 'purple' : 'purple',
         }
-        else {
-            style = { color: "tomato"}
-        }
+        const classes = this.props.isLiked ? 'fa fa-heart' : 'fa fa-heart-o'
+
+
         return(
             <div className='col-md-3'>
                 <img src={this.props.src} alt={this.props.caption} />
                 <p>{this.props.caption}</p>
-                <i className="fa fa-heart" style={style} aria-hidden='true'></i>
+                <i className={classes} style={style} aria-hidden='true'></i>
             </div>
         );
     }
